a88c5046cb102774fe856afb39ad179826914714
Egor Mesyats <egor.mesyats@moovweb.com>
2021-06-15 15:52:23 +0300

    feat: configure prefetching explicitly

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -8,6 +8,9 @@ import WishlistButton from '@components/wishlist/WishlistButton'
 import usePrice from '@bigcommerce/storefront-data-hooks/use-price'
 import type { ProductNode } from '@bigcommerce/storefront-data-hooks/api/operations/get-all-products'
 
+// @ts-ignore
+import { Prefetch } from '@layer0/react';
+
 interface Props {
   className?: string
   product: ProductNode
@@ -39,62 +42,64 @@ const ProductCard: FC<Props> = ({
   })
 
   return (
-    <Link href={`/product${p.path}`}>
-      <a
-        className={cn(s.root, { [s.simple]: variant === 'simple' }, className)}
-      >
-        {variant === 'slim' ? (
-          <div className="relative overflow-hidden box-border">
-            <div className="absolute inset-0 flex items-center justify-end mr-8 z-20">
-              <span className="bg-black text-white inline-block p-3 font-bold text-xl break-words">
-                {p.name}
-              </span>
-            </div>
-            <Image
-              quality="85"
-              width={imgWidth}
-              sizes={imgSizes}
-              height={imgHeight}
-              layout={imgLayout}
-              loading={imgLoading}
-              priority={imgPriority}
-              src={p.images.edges?.[0]?.node.urlOriginal!}
-              alt={p.images.edges?.[0]?.node.altText || 'Product Image'}
-            />
-          </div>
-        ) : (
-          <>
-            <div className={s.squareBg} />
-            <div className="flex flex-row justify-between box-border w-full z-20 absolute">
-              <div className="absolute top-0 left-0 pr-16 max-w-full">
-                <h3 className={s.productTitle}>
-                  <span>{p.name}</span>
-                </h3>
-                <span className={s.productPrice}>{price}</span>
+    <Link href={`/product${p.path}`} passHref>
+      <Prefetch>
+        <a
+          className={cn(s.root, { [s.simple]: variant === 'simple' }, className)}
+        >
+          {variant === 'slim' ? (
+            <div className="relative overflow-hidden box-border">
+              <div className="absolute inset-0 flex items-center justify-end mr-8 z-20">
+                <span className="bg-black text-white inline-block p-3 font-bold text-xl break-words">
+                  {p.name}
+                </span>
               </div>
-              <WishlistButton
-                className={s.wishlistButton}
-                productId={p.entityId}
-                variant={p.variants.edges?.[0]!}
-              />
-            </div>
-            <div className={s.imageContainer}>
               <Image
                 quality="85"
-                src={src}
-                alt={p.name}
-                className={s.productImage}
                 width={imgWidth}
                 sizes={imgSizes}
                 height={imgHeight}
                 layout={imgLayout}
                 loading={imgLoading}
                 priority={imgPriority}
+                src={p.images.edges?.[0]?.node.urlOriginal!}
+                alt={p.images.edges?.[0]?.node.altText || 'Product Image'}
               />
             </div>
-          </>
-        )}
-      </a>
+          ) : (
+            <>
+              <div className={s.squareBg} />
+              <div className="flex flex-row justify-between box-border w-full z-20 absolute">
+                <div className="absolute top-0 left-0 pr-16 max-w-full">
+                  <h3 className={s.productTitle}>
+                    <span>{p.name}</span>
+                  </h3>
+                  <span className={s.productPrice}>{price}</span>
+                </div>
+                <WishlistButton
+                  className={s.wishlistButton}
+                  productId={p.entityId}
+                  variant={p.variants.edges?.[0]!}
+                />
+              </div>
+              <div className={s.imageContainer}>
+                <Image
+                  quality="85"
+                  src={src}
+                  alt={p.name}
+                  className={s.productImage}
+                  width={imgWidth}
+                  sizes={imgSizes}
+                  height={imgHeight}
+                  layout={imgLayout}
+                  loading={imgLoading}
+                  priority={imgPriority}
+                />
+              </div>
+            </>
+          )}
+        </a>
+      </Prefetch>
     </Link>
   )
 }
